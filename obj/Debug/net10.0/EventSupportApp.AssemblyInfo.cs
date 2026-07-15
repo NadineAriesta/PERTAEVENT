d6dd405885b8d21ecd5c73e3b5caf05a8a4b546d
NamaGithubKamu <nadine.ariesta@mhs.unsoed.ac.id>
2026-07-15 11:12:29 +0700

    Update Home page

--- obj/Debug/net10.0/EventSupportApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventSupportApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventSupportApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efea23b11a4ee9edcb0bad9c8ce9645ef03643d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventSupportApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventSupportApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
